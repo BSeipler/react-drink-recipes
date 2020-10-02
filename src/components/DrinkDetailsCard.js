56893947218aefb328afb2d78493ce22ec2a3a96
Blake Seipler <bseipler@yahoo.com>
2020-10-02 12:11:02 -0400

    first commit

--- src/components/DrinkDetailsCard.js
+++ src/components/DrinkDetailsCard.js
@@ -19,17 +19,6 @@ const DrinkDetailsCard = props => {
         <div className='font-bold text-lg mb-2'>Directions</div>
         <p className='text-gray-700 text-base'>{props.directions}</p>
       </div>
-      <div className='px-6 pt-4 pb-2'>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-          #photography
-        </span>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-          #travel
-        </span>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-          #winter
-        </span>
-      </div>
     </div>
   )
 }
